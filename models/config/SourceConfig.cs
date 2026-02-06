9a0ee91a19d1f43f9976c42d2bb23db2ec8cb62d
SiobhanNWD <siobhan@northwind.digital>
2026-02-06 18:53:33 +0200

    Census downloading working (havent tested municipality builder to see if i broke it)
    - Added FileSourceDownloader to download files from non-download specific links (from raw links)... +although File Source downloader seems to be a website downoader, so if your website only consists of a file (raw link) then it downloads the file
    -

--- models/config/SourceConfig.cs
+++ models/config/SourceConfig.cs
@@ -1,9 +1,9 @@
 namespace Hoeveel.Aggregator.Models.Config;
 
-public class SourceConfig
-{
-    public UifwSourceConfig Uifw { get; set; } = new();
+    public class SourceConfig
+    {
+        public UifwSourceConfig Uifw { get; set; } = new();
+        public CsvSourceConfig CensusProv { get; set; } = new();
+        public CsvSourceConfig CensusMuni { get; set; } = new();
+    }
 
-    public CsvSourceConfig CensusProv { get; set; } = new();
-    public CsvSourceConfig CensusMuni { get; set; } = new();
-}
